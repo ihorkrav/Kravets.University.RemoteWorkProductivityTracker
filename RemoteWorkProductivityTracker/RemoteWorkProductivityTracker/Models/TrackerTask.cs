5ec22ede794cffbe3e24476b76e08ec8ac02ab0a
IhorKrav <102153718+ihorkrav@users.noreply.github.com>
2024-05-01 08:35:30 +0300

    First commit

---
+++ RemoteWorkProductivityTracker/RemoteWorkProductivityTracker/Models/TrackerTask.cs
@@ -0,0 +1,11 @@
+﻿namespace RemoteWorkProductivityTracker.Models
+{
+    public class TrackerTask
+    {
+        public int Id { get; set; }
+        private string Title { get; set; }
+        private string Description { get; set; }
+        private bool Compleate { get; set; }
+
+    }
+}
